4b7775b4bd2c6701a8c4f73ccf2c4e4997769e29
Welsh <cwelsh7@cos-4a15762.cos.gatech.edu>
2026-08-20 10:56:45 -0400

    expression syntax

--- seekr2/modules/mmvt_cvs/mmvt_drmsd_path_cv.py
+++ seekr2/modules/mmvt_cvs/mmvt_drmsd_path_cv.py
@@ -162,7 +162,7 @@ class MMVT_dRMSD_Path_CV(MMVT_collective_variable):
         assert num_atoms1 > 0 and num_atoms2 > 0, "Both group1 and group2 must contain atoms."
         num_pairs = num_atoms1 * num_atoms2
 
-        exp_terms = [f"exp(-lam * drmsd_{i}**2)" for i in range(num_frames)]
+        exp_terms = [f"exp(-lam * drmsd_{i}^2)" for i in range(num_frames)]
         num_terms = [f"{i + 1} * {term}" for i, term in enumerate(exp_terms)]
         numerator = " + ".join(num_terms)
         denominator = " + ".join(exp_terms)
